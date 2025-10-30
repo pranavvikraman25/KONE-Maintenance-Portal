185892b7237a0ec9e04a27b90c56496ae0fd839e
Pranav Vikraman s s <pranav.vikraman.25@gmail.com>
2025-10-30 17:11:20 +0530

    Update 1_Trend_Analyzer.py

--- pages/1_Trend_Analyzer.py
+++ pages/1_Trend_Analyzer.py
@@ -29,7 +29,24 @@ def get_uploaded_file():
 #------changed code----------
 # ---------------- Page config ----------------
 st.set_page_config(page_title="CKPI Multi-KPI Analyzer", layout="wide")
+
+#------------------------33 to 41 new code added----------------------------------------------------------------------
+
+# Load shared CSS
+def load_kone_style():
+    with open("assets/kone_style.css") as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_kone_style()
+
 st.title("Trend Analysis for Different Equipments")
+
+st.markdown("""
+<div class="hero">
+    <h1>KPI Report Generator</h1>
+    <p>Filter, analyze, and export your KPI results for inspection documentation.</p>
+</div>
+""", unsafe_allow_html=True)
 
 # ---------------- Load Custom CSS (KONE Theme) ----------------
 def load_css(file_name):
