43fe7d1fdbd39a3cac0a04a2aa22f7edd66ef89d
Pranav Vikraman s s <pranav.vikraman.25@gmail.com>
2025-10-30 17:38:25 +0530

    Update 2_JSON_to_Excel.py

--- pages/2_JSON_to_Excel.py
+++ pages/2_JSON_to_Excel.py
@@ -183,3 +183,14 @@ with st.spinner("🔄 Reading uploaded file..."):
         summary = merged["Name"].value_counts().reset_index()
         summary.columns = ["KPI Name", "Count"]
         st.table(summary)
+
+
+st.markdown("""
+<div class="footer">
+    © 2025 KONE Digital Maintenance | Developed with ❤️ by 
+    <a href="https://www.linkedin.com/in/pranav-vikraman-322020242/" target="_blank">
+        PRANAV VIKRAMAN S S
+    </a>
+</div>
+""", unsafe_allow_html=True)
+
